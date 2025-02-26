b38fc4fbbd4e9eee6e7f0fdbfdb7daad7733eb05
KevinJustin1104 <108638169+KevinJustin1104@users.noreply.github.com>
2025-02-27 00:41:50 +0800

    voucher env validation and add exit warning

---
+++ src/components/VoucherCard.tsx
@@ -0,0 +1,121 @@
+import { useState, useRef } from "react";
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonButton,
+  IonInput,
+  IonItem,
+} from "@ionic/react";
+import { useHistory } from "react-router-dom";
+
+const VoucherCard = ({ onClose }: any) => {
+  const history = useHistory();
+  const [voucherError, setVoucherError] = useState("");
+  const voucherInputRef = useRef("");
+
+  const validVouchers = [
+    process.env.VOUCHER_1,
+    process.env.VOUCHER_2,
+    process.env.VOUCHER_3,
+    process.env.VOUCHER_4,
+    process.env.VOUCHER_5,
+  ];
+
+  const handleApply = () => {
+    if (validVouchers.includes(voucherInputRef.current)) {
+      localStorage.setItem("isCorrectVoucher", "true");
+      setVoucherError("");
+      onClose();
+    } else {
+      setVoucherError(
+        "The voucher you entered is incorrect or does not exist."
+      );
+    }
+  };
+
+  const handleCancel = () => {
+    localStorage.removeItem("isCorrectVoucher");
+    history.goBack();
+    onClose();
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <IonCard className="p-6 shadow-lg w-96 bg-white rounded-lg">
+        <IonCardHeader className="text-xl font-semibold text-center text-yellow-600 mb-4">
+          Enter Voucher Code
+        </IonCardHeader>
+        <IonCardContent>
+          {/* <IonItem
+            style={{
+              "--background": "#e5e7eb",
+            }}
+          >
+            <IonInput
+              label="Floating label"
+              labelPlacement="floating"
+              placeholder="Enter text"
+              style={{
+                backgroundColor: "#e5e7eb", // Light gray
+                padding: "8px",
+                border: "none",
+                borderRadius: "4px",
+                width: "100%",
+                color: "black",
+              }}
+            />
+          </IonItem> */}
+
+          <IonItem
+            style={{
+              "--background": "#e5e7eb",
+              "--padding-start": "12px",
+              height: "60px",
+            }}
+          >
+            <IonInput
+              label="Click to type your voucher code"
+              labelPlacement="floating"
+              placeholder="Enter your voucher code"
+              onIonInput={(e) => {
+                voucherInputRef.current = e.detail.value?.trim() || "";
+                if (!voucherInputRef.current) {
+                  setVoucherError(""); // Clear error when input is cleared
+                }
+              }}
+              className=" p-2 rounded-md w-full mb-4 focus:outline-none focus:ring-2 focus:ring-yellow-200 text-sm text-black"
+            ></IonInput>
+          </IonItem>
+
+          {voucherError && (
+            <p className="text-red-600 text-center mt-2 text-sm">
+              {voucherError}
+            </p>
+          )}
+          <div className="flex justify-end gap-3 mt-6">
+            <IonButton
+              fill="solid"
+              color="default"
+              onClick={handleCancel}
+              className="w-24 text-sm font-medium text-yellow-600  rounded-md transition-all duration-200 hover:bg-gray-100 hover:border-yellow-600"
+            >
+              Cancel
+            </IonButton>
+
+            <IonButton
+              fill="solid"
+              color="warning"
+              onClick={handleApply}
+              className="w-24 text-sm font-medium text-white  rounded-md transition-all duration-200 hover:!bg-yellow-600 active:opacity-80"
+            >
+              Apply
+            </IonButton>
+          </div>
+        </IonCardContent>
+      </IonCard>
+    </div>
+  );
+};
+
+export default VoucherCard;
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
 // https://vitejs.dev/config/
+
+dotenv.config();
+
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), legacy()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+  define: {
+    "process.env": process.env,
+  },
+});
